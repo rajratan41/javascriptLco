a3e6acd052cfebda23cef8c8a07c3554da34a25a
Raj Ratan <raj808341@gmail.com>
2022-11-24 19:34:35 +0530

    adding inheritence example

--- 06NotAdvanceish/06classjs.js
+++ 06NotAdvanceish/06classjs.js
@@ -14,14 +14,36 @@ class user {
     getCourseList () {
         return this.#courseList;
     }
+
+    login() {
+        return "You are logged in";
+    }
 }
 
+class subAdmin extends user {
+    constructor (name, email) {
+        super(name, email);
+    }
+    getAdminInfo() {
+        return "I am Sub-Admin"
+    }
+    login() {
+        return "login for admin only"
+    } 
+}
 
 module.exports = user;
 
 
-const rock = new user("rock", "roack@rock.com");
-console.log(rock.getInfo());
-rock.enrollCourse("Angular Bootcamp");
-console.log(rock.getCourseList());
-console.log(rock.CourseList);
+// const rock = new user("rock", "roack@rock.com");
+// console.log(rock.getInfo());
+// rock.enrollCourse("Angular Bootcamp");
+// console.log(rock.getCourseList());
+// console.log(rock.CourseList);
+
+
+const tom = new subAdmin("tom", "tom@jerry.com");
+console.log(tom.getAdminInfo());
+console.log(tom.login());
+
+console.log(tom.getInfo());
